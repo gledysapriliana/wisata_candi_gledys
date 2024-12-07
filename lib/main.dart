0c19d74a90ac5bdf8f347b6bce94d44c8983bcab
gledysapriliana <gledysaprill@gmail.com>
2024-12-07 08:05:49 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ void main() {
   runApp(MaterialApp(
       initialRoute: '/',
       routes:{
-        '/':(context) => SignUpScreen(),
-        '/SignInScreen' : (context) => SignInscreen(),
-        // '/SignUpScreen' : (context) => SignUpScreen(),
+        '/':(context) => HomeScreen(),
+        '/SignInScreen' : (context) => SignInScreen(),
+        '/SignUpScreen' : (context) => SignUpScreen(),
       }
   ));
 }
@@ -28,30 +28,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Wisata Candi',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.deepPurple),
-          titleTextStyle: TextStyle(
-            color: Colors.deepPurple,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+        title: 'Wisata Candi',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.deepPurple),
+            titleTextStyle: TextStyle(
+              color: Colors.deepPurple,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+            primary: Colors.deepPurple,
+            surface: Colors.deepPurple[50],
+          ),
+          useMaterial3: true,
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
-          primary: Colors.deepPurple,
-          surface: Colors.deepPurple[50],
-        ),
-        useMaterial3: true,
-      ),
-      home: MainScreen(),
-      initialRoute: '/',
-      routes: {
-        '/homescreen': (context) => const HomeScreen(),
-        '/signin': (context) => SignInscreen(),
-        '/signup': (context) => SignUpScreen(),
-      },
+        home: MainScreen(),
+        initialRoute: '/',
+        routes:{
+          '/':(context) => HomeScreen(),
+          '/SignInScreen' : (context) => SignInScreen(),
+          '/SignUpScreen' : (context) => SignUpScreen(),
+        }
     );
   }
 }
@@ -81,35 +80,35 @@ class _MainScreenState extends State<MainScreen> {
       // TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
       bottomNavigationBar: Theme(
         // TODO: 4. Buat data dan child dari Theme
-          data: Theme.of(context).copyWith(
+        data: Theme.of(context).copyWith(
             canvasColor: Colors.deepPurple[50]
-          ),
-          child: BottomNavigationBar(
-            selectedItemColor: Colors.deepPurple,
-            unselectedItemColor: Colors.deepPurple[100],
-            showUnselectedLabels: true,
-            currentIndex: _currentIndex,
-            onTap: (index){
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home, color: Colors.deepPurple),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search, color: Colors.deepPurple),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite, color: Colors.deepPurple),
-                label: 'Favorite',
-              ),
-            ],
-          ),
-     ),
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showUnselectedLabels: true,
+          currentIndex: _currentIndex,
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.deepPurple),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.deepPurple),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, color: Colors.deepPurple),
+              label: 'Favorite',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
